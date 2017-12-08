d0ef227654c3eda35f02b67ea92c552e80dc16c0
FaizalFaaz <ahammedfaizal619@gmail.com>
2017-12-08 15:28:15 -0500

    first commit

---
+++ MvcMusicStore-Thu-F2017/Controllers/ShoppingCartController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using MvcMusicStore_Thu_F2017.Models;
+
+namespace MvcMusicStore_Thu_F2017.Controllers
+{
+    public class ShoppingCartController : Controller
+    {
+        // db
+        MusicStoreModel db = new MusicStoreModel();
+
+        // GET: ShoppingCart
+        public ActionResult Index()
+        {
+            // get current cart
+            var cart = ShoppingCart.GetCart(this.HttpContext);
+
+            // set up viewmodel
+            var viewModel = new ShoppingCartViewModel
+            {
+                CartItems = cart.GetItems(),
+                CartTotal = cart.GetTotal()
+            };
+
+            // pass populated Cart viewmodel to the view
+            return View(viewModel);
+        }
+
+        // GET: AddToCart/5
+        public ActionResult AddToCart(int AlbumId)
+        {
+            // Get current cart
+            var cart = ShoppingCart.GetCart(this.HttpContext);
+
+            // Get the selected album
+            var album = db.Albums.SingleOrDefault(a => a.AlbumId == AlbumId);
+
+            // Add album to the cart
+            cart.AddToCart(album);
+
+            // Redirect to the cart page (/ShoppingCart/Index)
+            return RedirectToAction("Index");
+        }
+    }
+}
